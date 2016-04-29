748f6de5b5150213239f184e038c58c2e9b18ad7
Stuart <stuart@localhost.localdomain>
2016-04-29 09:47:48 +0100

    commiting all

---
+++ build/CMakeFiles/tornado.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tornado.dir/state.cpp.o"
+  "CMakeFiles/tornado.dir/geometry.cpp.o"
+  "CMakeFiles/tornado.dir/main.cpp.o"
+  "tornado.pdb"
+  "tornado"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tornado.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
