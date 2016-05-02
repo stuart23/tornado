2e03d74a9ac59b066f668a8fc7bdbea49a302e81
Stuart Buckingham <stuart.buckingham@gmail.com>
2016-05-02 21:36:21 +0100

    cross sections

--- mesh.cxx
+++ mesh.cxx
@@ -11,8 +11,12 @@ Mesh::Mesh(TopoDS_Shape wing, int spanwise_divisions)
   for (float y_station = y_extents[0]; y_station <= y_extents[1]; y_station += (y_extents[1]-y_extents[0])/spanwise_divisions)
   {
     gp_Pln section_plane = gp_Pln(gp_Pnt(0.0,y_station,0.0), gp_Dir(0,1,0));
-    BRepAlgoAPI_Section a = BRepAlgoAPI_Section(wing, section_plane);
+    BRepAlgoAPI_Section section = BRepAlgoAPI_Section(wing, section_plane);
+    //std::array<float, 2> chord = Step::getXExtents(section.Shape());
     //sections.push_back(BRepAlgoAPI_Section(wing, section_plane, true));
-    std::cout << y_station << "\n";
+    //std::cout << "station: " << y_station << " x min " << chord[0] << " x max " << chord[1] << "\n";
+    std::cout << section.Shape().ShapeType() << "\n";
+    std::cout << (section.Shape().ShapeType() == TopAbs_COMPOUND ) << "\n";
+    //std::cout << "a";
   }
 }
