581a938a01fce4c78ab157996bfc1cde922d26fa
Stuart Buckingham <stuart.buckingham@gmail.com>
2016-05-04 23:53:06 +0100

    converted profiles to arma

--- mesh.cxx
+++ mesh.cxx
@@ -12,24 +12,25 @@
 #include <TopoDS_Edge.hxx>
 #include <BRepAdaptor_CompCurve.hxx>
 #include <gp_Pnt.hxx>
-#include <cstring>
-
 
 Mesh::Mesh(TopoDS_Shape input_wing)
 {
   wing = input_wing;
-  createProfiles(30, 30);
+  createProfiles(4, 4);
 }
 
-void Mesh::createProfiles(int spanwise_divisions, int chord_divisions)
+void Mesh::createProfiles(int input_spanwise_divisions, int input_chord_divisions)
 {
-  profiles = boost::extents[spanwise_divisions][chord_divisions][3];
+  spanwise_divisions = input_spanwise_divisions;
+  chord_divisions = input_chord_divisions;
+  
+  arma::Cube<double> profiles(spanwise_divisions + 1, chord_divisions, 3);
   std::array<float, 2> y_extents = Step::getYExtents(wing);
   std::vector<BRepAlgoAPI_Section> sections;
   
   float delta_y = (y_extents[1]-y_extents[0])/spanwise_divisions;
   float y_location = y_extents[0];
-  for (int span_step = 0; span_step <= spanwise_divisions; span_step++)
+  for (int span_step = 0; span_step <= spanwise_divisions + 1; span_step++)
   {
     gp_Pln section_plane = gp_Pln(gp_Pnt(0.0,y_location,0.0), gp_Dir(0,1,0));
     BRepAlgoAPI_Section section = BRepAlgoAPI_Section(wing, section_plane);
@@ -56,27 +57,31 @@ void Mesh::createProfiles(int spanwise_divisions, int chord_divisions)
       for (int chord_step = 0; chord_step < chord_divisions; chord_step++)
       {
 	comp_curve.D0(chord_location, point);
-	std::memcpy(profiles[span_step][chord_step], {(double) point.X(), (double) point.Y(), (double) point.Z()}, 3*sizeof(double));
+	// Theres probably a better way to do this!!!
+	profiles(span_step, chord_step, 0) = (double) point.X();
+	profiles(span_step, chord_step, 1) = (double) point.Y();
+	profiles(span_step, chord_step, 2) = (double) point.Z();
 	chord_location += delta_chord_parameter;
       }
-      profiles.push_back(profile);
     }
     y_location += delta_y;
   }
+  cout << profiles(0,0,0);
+  profiles.print();
 }
 
 void Mesh::createQuads()
 {
+  /*Need to check if profiles created first...
   if ( profiles.size == 0 )
   {
     throw std::invalid_argument( "Quads cannot be generated until profiles have first been created." );
-  }
-  int spanwise_divisions = profiles.size();
-  int chordwise_divisions = profiles[0].size();
+  }*/
   
-  int number_of_quads = (spanwise_divisions - 1) * (chordwise_divisions - 1);
+  int number_of_quads = (spanwise_divisions - 1) * (chord_divisions - 1);
   quads.resize(number_of_quads);
   for (int index = 0; index < number_of_quads; index++)
   {
-      
+    
+  }
 }
--- mesh.h
+++ mesh.h
@@ -1,13 +1,17 @@
 #include "iostream"
 #include "opencascade/STEPControl_Reader.hxx"
-#include "boost/multi_array.hpp"
+#include <armadillo>
 
 class Mesh
 {
 public:
   Mesh(TopoDS_Shape wing_argument);
-  void createProfiles(int spanwise_divisions, int chord_divisions);
+  void createProfiles(int input_spanwise_divisions, int input_chord_divisions);
+  void createQuads();
+  
   TopoDS_Shape wing;
-  boost::multi_array<double, 3> profiles;
+  arma::Cube<double> profiles();
   std::vector<std::array<std::array<double, 3>, 4>> quads;
+  int spanwise_divisions;
+  int chord_divisions;
 };
