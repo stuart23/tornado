f3084e6e7ab5b787262b97739f58fef2a136f99d
Stuart Buckingham <stuart.buckingham@gmail.com>
2016-05-05 01:08:30 +0100

    tried to take code into seperate method for creating comp edge from section. NOT WORKING due to pointer probblems

--- mesh.cxx
+++ mesh.cxx
@@ -1,6 +1,5 @@
 #include "mesh.h"
 #include "step.h"
-#include <BRepAlgoAPI_Section.hxx>
 #include <gp_Pln.hxx>
 #include <Bnd_Box.hxx>
 #include <BRepBndLib.hxx>
@@ -10,7 +9,6 @@
 #include <TopoDS.hxx>
 #include <TopoDS_Wire.hxx>
 #include <TopoDS_Edge.hxx>
-#include <BRepAdaptor_CompCurve.hxx>
 #include <gp_Pnt.hxx>
 
 Mesh::Mesh(TopoDS_Shape input_wing)
@@ -33,41 +31,53 @@ void Mesh::createProfiles(int input_spanwise_divisions, int input_chord_division
   for (int span_step = 0; span_step <= spanwise_divisions + 1; span_step++)
   {
     gp_Pln section_plane = gp_Pln(gp_Pnt(0.0,y_location,0.0), gp_Dir(0,1,0));
+    gp_Pnt point;
     BRepAlgoAPI_Section section = BRepAlgoAPI_Section(wing, section_plane);
-
-    Handle(TopTools_HSequenceOfShape) edges = new TopTools_HSequenceOfShape();
-    Handle(TopTools_HSequenceOfShape) wires = new TopTools_HSequenceOfShape();
-    TopExp_Explorer explorer;
-    for (explorer.Init(section.Shape(), TopAbs_EDGE); explorer.More(); explorer.Next())
+    BRepAdaptor_CompCurve comp_curve;
+    std::cout << "hERE"  ;
+    if ( getSectionEdges(section, &comp_curve) )
     {
-      edges->Append(TopoDS::Edge(explorer.Current()));
-    }
-    
-    ShapeAnalysis_FreeBounds::ConnectEdgesToWires(edges, Precision::Confusion(), Standard_False, wires);
-    
-    if ( wires->Length() > 0 )
-    {
-      TopoDS_Wire first_wire = TopoDS::Wire(wires->Value(1));
-      BRepAdaptor_CompCurve comp_curve = BRepAdaptor_CompCurve(first_wire, Standard_False);
-      gp_Pnt point;
-      
+      int k;
       double delta_chord_parameter = (comp_curve.LastParameter() - comp_curve.FirstParameter()) / chord_divisions;
       double chord_location = comp_curve.FirstParameter();
-      
+    
       for (int chord_step = 0; chord_step < chord_divisions; chord_step++)
       {
-	comp_curve.D0(chord_location, point);
-	// Theres probably a better way to do this!!!
-	profiles(span_step, chord_step, 0) = (double) point.X();
-	profiles(span_step, chord_step, 1) = (double) point.Y();
-	profiles(span_step, chord_step, 2) = (double) point.Z();
-	chord_location += delta_chord_parameter;
+        comp_curve.D0(chord_location, point);
+        // Theres probably a better way to do this!!!
+        profiles(span_step, chord_step, 0) = (double) point.X();
+        profiles(span_step, chord_step, 1) = (double) point.Y();
+        profiles(span_step, chord_step, 2) = (double) point.Z();
+        chord_location += delta_chord_parameter;
       }
     }
     y_location += delta_y;
   }
-  cout << profiles(0,0,0);
   profiles.print();
+}
+
+bool Mesh::getSectionEdges(BRepAlgoAPI_Section section, BRepAdaptor_CompCurve* comp_curve)
+{
+  Handle(TopTools_HSequenceOfShape) edges = new TopTools_HSequenceOfShape();
+  Handle(TopTools_HSequenceOfShape) wires = new TopTools_HSequenceOfShape();
+  TopExp_Explorer explorer;
+  for (explorer.Init(section.Shape(), TopAbs_EDGE); explorer.More(); explorer.Next())
+  {
+    edges->Append(TopoDS::Edge(explorer.Current()));
+  }
+    
+  ShapeAnalysis_FreeBounds::ConnectEdgesToWires(edges, Precision::Confusion(), Standard_False, wires);
+  int j = wires->Length();
+  if ( wires->Length() == 0 )
+  {
+    return false;
+  }
+  else
+  {
+    TopoDS_Wire first_wire = TopoDS::Wire(wires->Value(1));
+    //comp_curve->Initialize(first_wire, Standard_False);
+    return false;
+  }
 }
 
 void Mesh::createQuads()
--- mesh.h
+++ mesh.h
@@ -1,5 +1,7 @@
 #include "iostream"
 #include "opencascade/STEPControl_Reader.hxx"
+#include <BRepAlgoAPI_Section.hxx>
+#include <BRepAdaptor_CompCurve.hxx>
 #include <armadillo>
 
 class Mesh
@@ -14,4 +16,7 @@ public:
   std::vector<std::array<std::array<double, 3>, 4>> quads;
   int spanwise_divisions;
   int chord_divisions;
+  
+private:
+  bool getSectionEdges(BRepAlgoAPI_Section section, BRepAdaptor_CompCurve* comp_curve);
 };
